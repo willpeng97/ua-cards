9d6e9114e42902093432b0e429020d1bb10a1c79
willpeng <willpeng97@gmail.com>
2025-05-18 21:23:13 +0800

    .

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -101,10 +101,10 @@ const ProductCode = styled.div`
 	font-size: 0.9rem;
 	color: #666;
 	margin: 0.5rem 0;
-	display: inline-block;
-	text-overflow: ellipsis;
-	overflow: hidden;
-	white-space: nowrap;
+	display: flex;
+	align-items: center;
+	height: 2rem;
+	line-height: 1.1;
 
 	@media (max-width: 768px) {
 		font-size: 0.8rem;
